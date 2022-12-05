4b26767ac8f8eb46051f4c7e58c5fbd9fb4c45bd
WilfredoEscS <wilfredosorto75@gmail.com>
2022-12-05 15:19:38 -0600

    Clase 02 completada

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deleteIcon.js';
-
-const btn = document.querySelector('[data-form-btn]');
-
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  const list = document.querySelector('[data-list]');
-  const task = document.createElement('li');
-  task.classList.add('card');
-  input.value = '';
-  //backticks
-  const taskContent = document.createElement('div');
-
-  const titleTask = document.createElement('span');
-  titleTask.classList.add('task');
-  titleTask.innerText = value;
-  taskContent.appendChild(checkComplete());
-  taskContent.appendChild(titleTask);
-  // task.innerHTML = content;
-
-  task.appendChild(taskContent);
-  task.appendChild(deleteIcon());
-  list.appendChild(task);
+const btn = document.querySelector("[data-form-btn]");
+const createTask = (e) => {
+  e.preventDefault();
+  const input = document.querySelector("[data-form-input]");
+  console.log(input.value);
 };
 
-//Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+console.log(btn);
+
+//Muestra el mensaje en la consola al hacer clic en el boton
+btn.addEventListener("click", createTask);
