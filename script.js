359a34cf77d657563c5233bbf42c379d917de91a
WilfredoEscS <wilfredosorto75@gmail.com>
2022-12-05 18:31:43 -0600

    Clase 03 completada

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector("[data-form-input]");
-  console.log(input.value);
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  const value = input.value;
+  const content = `
+  <div>
+    <i class="fas fa-check-square completedIcon icon"></i>
+    <span class="task">${value}</span>
+    <i class="fas fa-check-squareX"></i>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>
+`;
+  task.classList.add("card");
+  input.value = "";
+  task.innerHTML = content;
+  list.appendChild(task);
+  console.log(content);
 };
 
 console.log(btn);
