80ba4b1605572ec54d4fc68679447cb34df3eaec
WilfredoEscS <wilfredosorto75@gmail.com>
2022-12-06 21:06:43 -0600

    Clase 04 completada

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-const btn = document.querySelector("[data-form-btn]");
-const createTask = (e) => {
-  e.preventDefault();
-  const input = document.querySelector("[data-form-input]");
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  const value = input.value;
-  const content = `
-  <div>
-    <i class="fas fa-check-square completedIcon icon"></i>
-    <span class="task">${value}</span>
-    <i class="fas fa-check-squareX"></i>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>
-`;
-  task.classList.add("card");
-  input.value = "";
-  task.innerHTML = content;
-  list.appendChild(task);
-  console.log(content);
-};
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
+  const createTask = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    const taskContent = document.createElement("div");
+    const taskTitle = document.createElement("span");
+    const value = input.value;
 
-console.log(btn);
+    task.classList.add("card");
+    taskTitle.classList.add("task");
+    list.appendChild(task);
+    task.appendChild(taskContent);
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(taskTitle);
+    taskTitle.textContent = value;
+    input.value = "";
+  };
 
-//Muestra el mensaje en la consola al hacer clic en el boton
-btn.addEventListener("click", createTask);
+  //Muestra el mensaje en la consola al hacer clic en el boton
+  btn.addEventListener("click", createTask);
+
+  //Funciones flecha
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (e) => {
+    const element = e.target;
+    element.classList.toggle("far");
+    element.classList.toggle("fa-square");
+    element.classList.toggle("fas");
+    element.classList.toggle("fa-check-square");
+    element.classList.toggle("completeIcon");
+  };
+})();
